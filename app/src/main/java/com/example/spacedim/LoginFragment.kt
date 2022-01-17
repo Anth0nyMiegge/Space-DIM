762fa2105bb731aecfb07c5ee9c1c071f011a3d9
Ben4600 <marques.benjaminb@gmail.com>
2022-01-17 14:58:07 +0100

    Navigation between Lobby And Login is Okay, starting fragment lobbyJoined + binding is added

--- app/src/main/java/com/example/spacedim/LoginFragment.kt
+++ app/src/main/java/com/example/spacedim/LoginFragment.kt
@@ -5,13 +5,34 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
+import com.example.spacedim.databinding.FragmentLoginBinding
 
-class LoginFragment : Fragment() {
+
+class LoginFragment : Fragment(){
+
+    private var _binding: FragmentLoginBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-            // Inflate the layout for this fragment
-            return inflater.inflate(R.layout.fragment_login, container, false)
+    ): View {
+
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+
+        val buttonLobby = binding.buttonLobby
+
+        buttonLobby.setOnClickListener {
+            view?.findNavController()?.navigate(R.id.login_to_lobby)
         }
+
+
+        return binding.root
     }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
