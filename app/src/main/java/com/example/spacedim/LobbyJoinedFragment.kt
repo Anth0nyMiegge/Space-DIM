35586eafaf8ad72286b5e8f36053baf67f75de81
Ben4600 <marques.benjaminb@gmail.com>
2022-01-18 14:15:32 +0100

    Multiples Changes and adding LobbyJoinedFragment in the LobbyFragment

--- app/src/main/java/com/example/spacedim/LobbyJoinedFragment.kt
+++ app/src/main/java/com/example/spacedim/LobbyJoinedFragment.kt
@@ -5,21 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.spacedim.databinding.FragmentLobbyBinding
+import com.example.spacedim.databinding.FragmentLobbyJoinedBinding
 
-class LobbyFragment : Fragment() {
+class LobbyJoinedFragment : Fragment() {
 
-    private var _binding: FragmentLobbyBinding? = null
+    private var _binding: FragmentLobbyJoinedBinding? = null
     private val binding get() = _binding!!
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val lobbyJoinedFragment = LobbyJoinedFragment()
-        addChildFragment(lobbyJoinedFragment, R.id.fragment_container)
-
-        _binding = FragmentLobbyBinding.inflate(inflater, container, false)
+        _binding = FragmentLobbyJoinedBinding.inflate(inflater, container, false)
 
         return binding.root
     }
